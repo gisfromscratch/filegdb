9066758ce4be467c8f9d03c41af2e2a50714de48
J.T <gisfromscratch@live.de>
2017-01-15 16:47:17 +0100

    Fighting the disposd exception

--- src/FileGDB.Managed.Testing/FileGDB.Managed.Testing/GeodatabaseTestSuite.cs
+++ src/FileGDB.Managed.Testing/FileGDB.Managed.Testing/GeodatabaseTestSuite.cs
@@ -18,21 +18,82 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Esri.FileGDB;
 using System.IO;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace FileGDB.Managed.Testing
 {
     [TestClass]
     public class GeodatabaseTestSuite
     {
+        private const string GeodatabaseName = @"EmptyGeodatabase.gdb";
+
+        [TestInitialize]
+        public void TestIntialize()
+        {
+            if (Directory.Exists(GeodatabaseName))
+            {
+                Geodatabase.Delete(GeodatabaseName);
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (Directory.Exists(GeodatabaseName))
+            {
+                Geodatabase.Delete(GeodatabaseName);
+            }
+        }
+
         [TestMethod]
         public void TestCreateAndDeleteGeodatabase()
         {
-            const string geodatabaseName = @"EmptyGeodatabase.gdb";
-            using (var gdb = Geodatabase.Create(geodatabaseName))
+            using (var gdb = Geodatabase.Create(GeodatabaseName))
             {
                 gdb.Close();
+            }
+        }
 
-                Geodatabase.Delete(geodatabaseName);
+        [TestMethod]
+        public void TestCreateGeodatabaseAndForcingTableCreateException()
+        {
+            using (var gdb = Geodatabase.Create(GeodatabaseName))
+            {
+                const string tableName = @"TestTable";
+                var fields = new List<FieldDef>(1);
+                const string invalidFieldName = @"1_OID";
+                var oidField = new FieldDef { Name = invalidFieldName, Type = FieldType.OID };
+                fields.Add(oidField);
+                var exceptionRaised = false;
+                try
+                {
+                    using (var table = gdb.CreateTable(tableName, fields.ToArray(), string.Empty))
+                    {
+                    }
+                }
+                catch (FileGDBException)
+                {
+                    // Avoid disposed exception
+                    // the table instance is created internally without having a native table handle
+                    // because of the raised exception you are not able to access this created table instance
+                    // when this pending table instance is finalized after the geodatabase was disposed
+                    // a diposed exception is thrown
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                    exceptionRaised = true;
+                }
+                Assert.IsTrue(exceptionRaised, @"A table having an invalid field name should not be created!");
+
+                // Try again
+                oidField.Name = @"OID";
+                using (var table = gdb.CreateTable(tableName, fields.ToArray(), string.Empty))
+                {
+                    table.Close();
+                }
+
+                gdb.Close();
             }
         }
     }
