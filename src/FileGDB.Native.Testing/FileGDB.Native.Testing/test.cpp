9e37cb20b404ef82303f6e5474739a4a49a5e0da
J.T <gisfromscratch@live.de>
2019-05-06 18:33:47 +0200

    Create empty geodatabase and create empty table.

--- src/FileGDB.Native.Testing/FileGDB.Native.Testing/test.cpp
+++ src/FileGDB.Native.Testing/FileGDB.Native.Testing/test.cpp
@@ -16,11 +16,43 @@
 
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
-	FileGDBAPI::Geodatabase gdb;
-	FileGDBAPI::OpenGeodatabase(L"", gdb);
+TEST(FileGDB, CreateFileGDB) {
+	using namespace FileGDBAPI;
+	Geodatabase gdb;
+	const std::wstring workspace_path = L"Data/NewlyCreated.gdb";
+	if (S_OK == OpenGeodatabase(workspace_path, gdb))
+	{
+		EXPECT_EQ(S_OK, CloseGeodatabase(gdb));
+		EXPECT_EQ(S_OK, DeleteGeodatabase(workspace_path));
+	}
+
+	EXPECT_EQ(S_OK, CreateGeodatabase(workspace_path, gdb)) << "Create file geodatabase.";
+	EXPECT_EQ(S_OK, CloseGeodatabase(gdb)) << "Close file geodatabase.";
+}
+
+TEST(FileGDB, CreateEmptyTable) {
+	using namespace FileGDBAPI;
+	Geodatabase gdb;
+	const std::wstring workspace_path = L"Data/EmptyTable.gdb";
+	if (S_OK == OpenGeodatabase(workspace_path, gdb))
+	{
+		EXPECT_EQ(S_OK, CloseGeodatabase(gdb));
+		EXPECT_EQ(S_OK, DeleteGeodatabase(workspace_path));
+	}
+
+	EXPECT_EQ(S_OK, CreateGeodatabase(workspace_path, gdb)) << "Create file geodatabase.";
+
+	FieldDef oidFieldDef;
+	oidFieldDef.SetAlias(L"OBJECTID");
+	oidFieldDef.SetName(L"OID");
+	oidFieldDef.SetType(FieldType::fieldTypeOID);
+	std::vector<FieldDef> fields;
+	fields.push_back(oidFieldDef);
+	Table empty_table;
+	EXPECT_EQ(S_OK, gdb.CreateTable(L"EmptyTable", fields, L"", empty_table)) << "Create empty table having OID.";
+	EXPECT_EQ(S_OK, gdb.CloseTable(empty_table)) << "Close empty table.";
+
+	EXPECT_EQ(S_OK, CloseGeodatabase(gdb)) << "Close file geodatabase.";
 }
 
 int main(int argc, wchar_t* argv[])
